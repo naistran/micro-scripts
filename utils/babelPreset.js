53bdb16155808f45ed3f208ffa4e836348bd7bb0
Nathan Tran <ntran013@gmail.com>
2016-12-09 17:48:28 +1100

    Update deps

--- utils/babelPreset.js
+++ utils/babelPreset.js
@@ -6,10 +6,6 @@ module.exports = function preset() {
         targets: {
           node: 'current',
         },
-        whitelist: [
-           'transform-es2015-destructuring', // whitelist and pin transform-object-rest-spread
-           'transform-es2015-parameters',    // because of this https://github.com/babel/babel/issues/4904
-         ],
       }],
       require.resolve('babel-preset-stage-1'),
       require.resolve('babel-preset-react'),
